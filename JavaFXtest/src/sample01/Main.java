d6bf40cb940c8475a65e93d38eac53dd8c7e7bdb
betel <betelgeuse525@gmail.com>
2012-12-20 08:47:37 +0900

    addGUI

---
+++ JavaFXtest/src/sample01/Main.java
@@ -0,0 +1,20 @@
+package sample01;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+	public void start(Stage stage) throws Exception{
+		stage.setTitle("SAMPLE");
+		Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	public static void main(String[] args){
+		launch(args);
+	}
+}
