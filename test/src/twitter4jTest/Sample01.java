de8dc5575eeba432fcbcdef499ad60102309841b
betel <betelgeuse525@gmail.com>
2013-01-12 03:03:11 +0900

    Edit:twitter4jtest

--- test/src/twitter4jTest/Sample01.java
+++ test/src/twitter4jTest/Sample01.java
@@ -1,5 +1,13 @@
 package twitter4jTest;
 
-public class Sample01 {
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
 
+public class Sample01 {
+	public void start(){
+		Twitter twitter = TwitterFactory.getSingleton();
+		Status status = twitter.updateStatus(latestStatus);
+		System.out.println("Successfully update the status to [" + status.getText() + "]." );
+	}
 }
