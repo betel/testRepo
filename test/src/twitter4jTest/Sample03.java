f794408b768753ccec7b15a747fa3fc64359b086
betel <betelgeuse525@gmail.com>
2013-01-13 07:11:08 +0900

    update:sample03

--- test/src/twitter4jTest/Sample03.java
+++ test/src/twitter4jTest/Sample03.java
@@ -1,14 +1,35 @@
 package twitter4jTest;
 
-import java.util.List;
-
-import twitter4j.*;
+import twitter4j.Status;
+import twitter4j.StatusAdapter;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+import twitter4j.User;
 
 public class Sample03 {
 	public static void main(String[] args){
 		try{
-			Twitter twitter = new TwitterFactory().getSingleton();
-			twitterstream.
+			TwitterStream twitterStream = TwitterStreamFactory.getSingleton();
+			twitterStream.addListener(new MyStatusAdapter());
+			twitterStream.sample();
+		}catch(Exception e){
+			e.printStackTrace();
 		}
+	}
+}
+
+class MyStatusAdapter extends StatusAdapter{
+	public void onStatus(Status status){
+		User user = status.getUser();
+
+		String strText = status.getText();
+		strText = strText.replaceAll("\r\n", " ");
+		strText = strText.replaceAll("\r", " ");
+		strText = strText.replaceAll("\n", " ");
+
+		strText = strText.replaceAll("\t", " ");
+
+		System.out.println(user.getScreenName()
+				+ "\t" + strText);
 	}
 }
