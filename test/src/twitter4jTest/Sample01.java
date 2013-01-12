c557a696cc20f021b0c6eab64a7857e0d55ea4ed
betel <betelgeuse525@gmail.com>
2013-01-12 19:51:23 +0900

    update sample01

--- test/src/twitter4jTest/Sample01.java
+++ test/src/twitter4jTest/Sample01.java
@@ -2,12 +2,22 @@ package twitter4jTest;
 
 import twitter4j.Status;
 import twitter4j.Twitter;
+import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 
 public class Sample01 {
 	public void start(){
 		Twitter twitter = TwitterFactory.getSingleton();
-		Status status = twitter.updateStatus(latestStatus);
-		System.out.println("Successfully update the status to [" + status.getText() + "]." );
+		try {
+			Status status = twitter.updateStatus("Hello!");
+			System.out.println("Successfully update the status to [" + status.getText() + "]." );
+		} catch (TwitterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+	}
+	
+	public static void main(String[] args){
+		new Sample01().start();
 	}
 }
